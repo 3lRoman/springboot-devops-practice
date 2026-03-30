3974771b0b51527a9192a9d29783fb5c1884f2f4
eroman03 <eroman@roshka.com>
2026-03-30 16:39:49 -0300

    Proyecto Spring Boot inicial

---
+++ Github_Actions/src/test/java/com/devops/Github_Actions/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.Github_Actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
